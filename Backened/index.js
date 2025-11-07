df7f1f78196d012af4103e2ba8802c8b1489c8b1
Kashish Singh <kashish2392004@gmail.com>
2025-11-07 23:38:36 +0530

    final changes for deployment

--- Backened/index.js
+++ Backened/index.js
@@ -7,7 +7,7 @@ import corsMiddleware from './cors.js';
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT =  process.env.PORT || 5000;
 const URI = process.env.MONGODB_URI;
 
 async function startserver() {
