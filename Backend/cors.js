5c3855dac3ff44597d1bf55ddb9c67401f06a16a
Kashish Singh <kashish2392004@gmail.com>
2025-11-08 09:05:33 +0530

    integrate code

--- Backend/cors.js
+++ Backend/cors.js
@@ -1,7 +1,8 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: [ "http://localhost:5173",
+    "https://codegenie-componentmaker-client.onrender.com"],
   credentials: true,
 };
 
