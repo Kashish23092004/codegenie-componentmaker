b1c950b3768692f72acce989c632a9b87e66f6d7
Kashish Singh <kashish2392004@gmail.com>
2026-03-30 22:19:08 +0530

    canges in routes

--- Backend/cors.js
+++ Backend/cors.js
@@ -2,8 +2,8 @@ import cors from 'cors';
 
 const corsOptions = {
   origin: [
-    'https://codegenie-componentmaker-gljf9aegf-kashish23092004s-projects.vercel.app/',
-    'http://localhost:5173',
+    'https://codegenie-componentmaker-gljf9aegf-kashish23092004s-projects.vercel.app',
+    'http://localhost:5173', 
     'http://localhost:5174'
   ], 
   credentials: true,
--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -17,7 +17,7 @@ const Signup = () => {
 
     try {
       // FORCED LOCALHOST FOR TESTING
-      const res = await fetch('https://codegenie-componentmaker-backend.onrender.com/api/users', {
+     const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ fullname, email, password })
